109811a7b7aff9c3c813fe403ea26e6e55e88fdd
William W Wold <wilcoe@rocketmail.com>
2016-11-09 12:41:15 -0800

    added file loading to histogram

--- wordHistogram.py
+++ wordHistogram.py
@@ -1,4 +1,7 @@
 
+import re
+import sys
+
 class Histogram:
 	
 	words={}
@@ -8,23 +11,32 @@ class Histogram:
 	
 	def addList(self, wordList):
 		for word in wordList:
-			if word in self.words:
-				self.words[word]+=1
-			else:
-				self.words[word]=1;
+			if word:
+				if word in self.words:
+					self.words[word]+=1
+				else:
+					self.words[word]=1;
 	
 	def addString(self, text):
-		wordList=text.split()
+		delimiters = " ", ",", ".", "\n", "\t", ";"
+		regexPattern = '|'.join(map(re.escape, delimiters))
+		wordList=re.split(regexPattern, text)
 		self.addList(wordList)
+	
+	def addFile(self, filename):
+		self.addString(open('/usr/share/dict/words', 'r').read())
 	
 	def getFrequency(self, word):
 		return self.words[word]
 
 def run():
 	hist=Histogram()
-	hist.addString("cat dog dog mouse cat cat.\ncat")
-	print("cat appears " + str(hist.getFrequency("cat")) + " times")
-	
+	if len(sys.argv)<3:
+		print("too few args")
+	else:
+		hist.addFile(sys.argv[1])
+		print("'" + sys.argv[2] + "' appears " + str(hist.getFrequency(sys.argv[2])) + " times")
+		print(hist.words)
 	
 
 if __name__ == '__main__':
