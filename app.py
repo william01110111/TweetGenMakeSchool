f0274fea331b83d1cf7c16b89e709682dafcb11c
William W Wold <wilcoe@rocketmail.com>
2016-11-14 14:21:03 -0800

    made it work?

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from wordHistogram import Histogram
 from randomWordSelector import RandomWordSelector
+import os
 
 from flask import Flask
 app = Flask(__name__)
@@ -18,5 +19,6 @@ def getRandWord():
 	return selector.getRandWord()
 
 if __name__ == "__main__":
-    app.run()
+	port= int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
 
